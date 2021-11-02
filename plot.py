1e40357beb026c0b1b56074f85ae81418938b394
Philipp Neufeld <pneufeld@pi5.physik.uni-stuttgart.de>
2021-11-02 18:01:08 +0100

    Changed plotscript back to the Test app

--- plot.py
+++ plot.py
@@ -2,6 +2,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-det, coeff = np.genfromtxt("build/apps/Rb87_EIT/data.txt", unpack=True)
+det, coeff = np.genfromtxt("build/apps/Test/data.txt", unpack=True)
 plt.plot(det, coeff)
 plt.show()
