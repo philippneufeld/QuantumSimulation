8f6935dee56410780d87f7fae3c4719819a5c0b9
Philipp Neufeld <pneufeld@pi5.physik.uni-stuttgart.de>
2022-01-12 19:57:41 +0100

    HDF5

--- apps/Test/main.cpp
+++ apps/Test/main.cpp
@@ -3,34 +3,24 @@
 #include <iostream>
 #include <QSim/Util/DataFile2.h>
 
+#include <H5Cpp.h>
+
 int main(int argc, const char* argv[])
 {
-    QSim::DataFileDriver driver;
-
-    std::cout << (driver.OpenFile("test.dat") ? "success" : "failure") << std::endl;
-
-#ifdef QSIM_PYTHON3
-    std::cout << "QSIM_PYTHON3" << std::endl;
-#endif
-
-    /*auto file = driver.GetOSHandle().lock();
-
-    std::cout << file->GetCursorPos() << std::endl;
-
-    std::cout << (file->SetCursorPos(2) ? "success" : "failure") << std::endl;
-    std::cout << file->GetCursorPos() << std::endl;
-
-    file->Write("aaa", 3);
-
-    std::cout << file->GetCursorPos() << std::endl;
+    H5::H5File file("test.h5", H5F_ACC_TRUNC);
     
-    std::cout << (file->SetCursorPos(20) ? "success" : "failure") << std::endl;
-    std::cout << file->GetCursorPos() << std::endl;
+    // Create the data space for the dataset.
+    hsize_t dims[2]; // dataset dimensions
+    dims[0] = 4;
+    dims[1] = 4;
+    H5::DataSpace dataspace(2, dims);
+    H5::DataSet dataset = file.createDataSet("test", H5::PredType::STD_I32BE, dataspace);
     
-    file->Write("bbb", 3);
-
-
-    std::cout << file->GetCursorPos() << std::endl;*/
+    int data[4][4]; // buffer for data to write
+    for (int j = 0; j < 4; j++)
+        for (int i = 0; i < 4; i++)
+            data[j][i] = i * 6 + j + 1;
+    dataset.write(data, H5::PredType::NATIVE_INT);
 
     return 0;
 }
