03acce21d69b357ada84618b214bfbe4b2fadfc2
Philipp Neufeld <pneufeld@pi5.physik.uni-stuttgart.de>
2022-01-12 09:52:32 +0100

    Datafile progress

--- apps/Test/main.cpp
+++ apps/Test/main.cpp
@@ -1,10 +1,32 @@
 // Philipp Neufeld, 2021-2022
 
+#include <iostream>
 #include <QSim/Util/DataFile2.h>
 
 int main(int argc, const char* argv[])
 {
     QSim::DataFileDriver driver;
+
+    std::cout << (driver.OpenFile("test.dat") ? "success" : "failure") << std::endl;
+
+    /*auto file = driver.GetOSHandle().lock();
+
+    std::cout << file->GetCursorPos() << std::endl;
+
+    std::cout << (file->SetCursorPos(2) ? "success" : "failure") << std::endl;
+    std::cout << file->GetCursorPos() << std::endl;
+
+    file->Write("aaa", 3);
+
+    std::cout << file->GetCursorPos() << std::endl;
+    
+    std::cout << (file->SetCursorPos(20) ? "success" : "failure") << std::endl;
+    std::cout << file->GetCursorPos() << std::endl;
+    
+    file->Write("bbb", 3);
+
+
+    std::cout << file->GetCursorPos() << std::endl;*/
 
     return 0;
 }
