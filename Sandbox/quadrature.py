cb03f485d078bfd468942e3ebc14eb398c5a23ba
Philipp Neufeld <philipp.neufeld@protonmail.com>
2022-03-13 16:15:48 +0100

    Grusi plot

--- Sandbox/quadrature.py
+++ Sandbox/quadrature.py
@@ -65,7 +65,7 @@ def adaptive2Helper(Iest, xs, f, a, dx, depth, f0, f2, f4, atol, rtol, maxdepth)
     Ierr = (I2-I1) / 15
     I = I2 + Ierr # equivalent to Boole's rule
 
-    if (depth > 1):
+    if (depth > 0):
         xs.append(a + 0.25*dx)
         xs.append(a + 0.75*dx)
 
@@ -93,7 +93,7 @@ def adaptive2(f, a, b, n, atol, rtol, maxdepth):
     Iest = abs((0.5*(fs[0]+fs[-1]) + np.sum(fs[1:-1]))*0.5*dx / secs)
     fevs = 1+2*secs
 
-    xs = []
+    xs = [a+0.5*i*dx for i in range(1+2*secs)]
     I, subFevs = adaptive2Helper(Iest, xs, f, a, dx, 1, fs[0], fs[1], fs[2], atol, rtol, maxdepth)
     fevs += subFevs
 
@@ -109,11 +109,10 @@ if __name__ == "__main__":
 
     a = -250
     b = 250
-    atol = 1e-9
-    rtol = 1e-7
+    atol = 1e-5
+    rtol = 1e-4
     depth = 100
-    n = 200
-    
+    n = 40
 
     # calculate exact solution
     xsp = sp.Symbol("x")
@@ -121,23 +120,42 @@ if __name__ == "__main__":
     print(f"Exact: {exact}")
 
     # adaptive 1
-    print("")
-    I1, fevs1, xs1 = adaptive1(func, a, b, n, atol, rtol, depth)
-    print(f"Adaptive1 {abs(I1 - exact):e} ({fevs1})")
-    x1 = np.linspace(a, b, fevs1)
-    print(f"Euler ({fevs1}): {abs(np.sum(func(x1)) * (b-a)/(fevs1-1) - exact):e}")
-    print(f"Simpson ({fevs1}): {abs(scipy.integrate.simpson(func(x1), x1) - exact):e}")
+    # print("")
+    # I1, fevs1, xs1 = adaptive1(func, a, b, n, atol, rtol, depth)
+    # print(f"Adaptive1 {abs(I1 - exact)/exact:e} ({fevs1})")
+    # x1 = np.linspace(a, b, fevs1)
+    # print(f"Euler ({fevs1}): {abs(np.sum(func(x1)) * (b-a)/(fevs1-1) - exact)/exact:e}")
+    # print(f"Simpson ({fevs1}): {abs(scipy.integrate.simpson(func(x1), x1) - exact)/exact:e}")
 
     # adaptive 2
     print("")
     I2, fevs2, xs2 = adaptive2(func, a, b, n, atol, rtol, depth)
-    print(f"Adaptive2 {abs(I2 - exact):e} ({fevs2})")
+    print(f"Adaptive2 {abs(I2 - exact)/exact:e} ({fevs2})")
     x2 = np.linspace(a, b, fevs2)
-    print(f"Euler ({fevs2}): {abs(np.sum(func(x2)) * (b-a)/(fevs2-1) - exact):e}")
-    print(f"Simpson ({fevs2}): {abs(scipy.integrate.simpson(func(x2), x2) - exact):e}")
+    print(f"Euler ({fevs2}): {abs(np.sum(func(x2)) * (b-a)/(fevs2-1) - exact)/exact:e}")
+    print(f"Simpson ({fevs2}): {abs(scipy.integrate.simpson(func(x2), x2) - exact)/exact:e}")
 
-    x = np.linspace(a, b, 1000)
-    plt.plot(x, func(x))
-    plt.plot(xs1, func(xs1), '.')
-    plt.plot(xs2, func(xs2), '.')
+    x = np.linspace(a, b, 10000)
+
+    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, gridspec_kw={'height_ratios': [4 ,1, 1]}, sharex=True, figsize=(700 // 72, 250 // 72))
+    # ax1 = fig.add_subplot(311)
+    # ax2 = fig.add_subplot(312, sharex=ax1)
+    # ax3 = fig.add_subplot(313, sharex=ax1)
+
+    ax1.plot(x * 6e0, func(x))
+    ax1.plot(xs2 * 6e0, func(xs2), '.', color="C2", label="Adaptive")
+    ax1.plot(x2 * 6e0, func(x2), 'x', color="C1", label="Simpson rule")
+    plt.setp(ax1.get_xticklabels(), visible=False)
+    ax1.legend()
+
+    ax2.plot(x2 * 6e0, np.zeros_like(x2), ".", color="C1")
+    ax2.set_yticks([])
+    plt.setp(ax2.get_xticklabels(), visible=False)
+
+    ax3.plot(xs2 * 6e0, np.zeros_like(xs2), ".", color="C2")
+    ax3.set_yticks([])
+    ax3.set_xlabel("Integration variable")
+    
+    fig.tight_layout()
+
     plt.show()
