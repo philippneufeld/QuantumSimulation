08268331b87e45156cd516fac3e5e1ea0957d287
Philipp Neufeld <pneufeld@pi5.physik.uni-stuttgart.de>
2022-04-08 16:48:11 +0200

    Work in progress

--- Plotting/NO_StarkMap/plot.py
+++ Plotting/NO_StarkMap/plot.py
@@ -1,57 +1,89 @@
 # Philipp Neufeld, 2021-2022
 
+from typing import Any, Tuple
+
 import os
-import argparse
 import tqdm
 import h5py
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as col
 
-def unitVec(i, n):
-    vec = np.zeros(n)
-    vec[i] = 1
-    return vec
+
+class NOStarkMap:
+
+    def __init__(self, path) -> None:
+        self.path = path
+
+    def generate_R_matrix(self, basis: np.ndarray) -> np.ndarray:
+        """Generate a matrix that shows which basis state belongs to which R manifold
+
+        Args:
+            basis (np.ndarray): basis states
+
+        Returns:
+            np.ndarray: Generated matrix
+        """
+        maxR = np.max([b[2] for b in basis])
+        rot_matrix = np.zeros((int(maxR)+1, len(basis)))
+        for i in range(len(basis)):
+            rot_matrix[int(basis[i, 2]), i] = 1
+        return rot_matrix
+
+    def process_datagroup_R(self, datagroup: Any, rot_matrix: np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        """Load the slice from 
+
+        Args:
+            datagroup (np.ndarray): HDF datagroup for the desired electric field
+            rot_matrix (np.ndarray): Matrix that specifies which basis belongs to which R manifold
+
+        Returns:
+            Tuple[np.ndarray, np.ndarray, np.ndarray]: electric fields, energies, colors
+        """
+        energies = np.array(datagroup["Energies"][::])
+        states = np.array(datagroup["States"][::])
+        eField = np.ones_like(energies) * float(datagroup.attrs["Electric_Field"])
+
+        # select color based on the R manifold
+        colors = np.array([col.to_rgb(f"C{np.argmax(np.sum(rot_matrix * np.square(s), axis=1))}") for s in states.T])
+
+        # match states to previous states
+        # max_overlap = np.argmax(np.matmul(statesT, prev_states), axis=0)
+        # eField = eField[max_overlap]
+        # energies = energies[max_overlap]
+        # colors = colors[max_overlap, :]
+        # states = states[:, max_overlap]
+
+        return eField, energies, colors
+
+    def plot(self):
+
+        # prepare data
+        plot_data = []
+        with h5py.File(self.path) as file:
+            rot_matrix = self.generate_R_matrix(file["Basis"][::])
+            for key in tqdm.tqdm([k for k in file.keys() if k.isdecimal() and k.endswith('1')]):
+                eField, energies, colors = self.process_datagroup_R(file[key], rot_matrix)
+                plot_data.append((eField, energies, colors))
+
+        # plot
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+
+        for eField, energies, colors in plot_data:
+            ax.scatter(eField, energies, 0.5, colors)
+
+        ax.set_xlim((0, 25))
+        ax.set_ylim((-66.5, -61.5))
+        ax.set_xlabel("Electric field (V / cm)")
+        ax.set_ylabel("Energy / $hc$ (cm${}^{-1}$)")
 
 if __name__ == '__main__':
 
     filename = "NOStarkMap_20220407-162632_calcc"
     dir_path = "/home/PI5/pneufeld/remote_home/Masterarbeit/06_StarkMap/03_NO/"
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-
-    with h5py.File(os.path.join(dir_path, filename + ".h5")) as file:
-        attrs = dict(file.attrs)
-        keys = list(file.keys())
-
-        basis = file["basis"][::]
-        maxR = np.max([b[2] for b in basis])
-        basisR = np.zeros((int(maxR)+1, len(basis)))
-        for i in range(len(basis)):
-            basisR[int(basis[i, 2]), i] = 1
-
-        print(attrs)
-        for key in tqdm.tqdm([k for k in keys if k.isdecimal()]):
-            try:
-                data = file[key]
-                energies = np.array(data["Energies"][::])
-                states = np.array(data["States"][::]).T
-                eField = np.ones_like(energies) * float(data.attrs["Electric_Field"])
-
-                colors = ["C0" for e in energies]
-                for i, ev in enumerate(states):
-                    tmp = basisR * ev[None, :]
-                    col_id = np.argmax(np.diag(np.matmul(tmp, tmp.T)))
-                    colors[i] = f"C{col_id}"
-
-                ax.scatter(eField, energies, 0.5, colors)
-            except Exception as e:
-                print(e)
-
-    ax.set_xlim((0, 25))
-    ax.set_ylim((-66.5, -61.5))
-
-    ax.set_xlabel("Electric field (V / cm)")
-    ax.set_ylabel("Energy / $hc$ (cm${}^{-1}$)")
+    stark_map = NOStarkMap(os.path.join(dir_path, filename + ".h5"))
+    stark_map.plot()
 
     plt.show()
--- apps/NO_StarkMap/main.cpp
+++ apps/NO_StarkMap/main.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <algorithm>
+#include <limits>
 #include <Eigen/Dense>
 
 #include <QSim/Constants.h>
@@ -31,24 +32,53 @@ unsigned int GetNumberOfCalcThreads()
 
 int main(int argc, const char* argv[])
 {
-    constexpr double dE = 3.5 * EnergyInverseCm_v;
+    // constexpr double dE = 3.5 * EnergyInverseCm_v;
+    constexpr double dE = 0.05 * EnergyInverseCm_v;
     
     NitricOxide molecule;
     RydbergDiatomicState_t state(38, 2, 2, 0, 0);
     DiatomicStarkMap starkMap(molecule, state, 30, 60, 3, dE);
-    
-    int cnt = starkMap.GetBasis().size();
-    std::cout << "Basis size: " << cnt << std::endl;
 
-    auto eField = VectorXd::LinSpaced(1500, 0.0, 25.0); // V cm^-1
+    // analyze basis
+    std::vector<RydbergDiatomicState_t> basis = starkMap.GetBasis();
+    int basisSize = basis.size();
+    int minN = std::numeric_limits<int>::max();
+    int maxN = std::numeric_limits<int>::min();
+    int maxR = 0;
+    int maxL = 0;
+    for (const auto& state: basis)
+    {
+        auto [n, l, R, N, mN] = state;
+        minN = std::min(minN, n);
+        maxN = std::max(maxN, n);
+        maxL = std::max(maxL, l);
+        maxR = std::max(maxR, R);
+    }
+    std::cout << "Basis size: " << basisSize << std::endl;
+
+    // generate character matrices
+    MatrixXd nCharMatrix(maxN - minN + 1, basisSize);
+    MatrixXd lCharMatrix(maxL + 1, basisSize);
+    MatrixXd RCharMatrix(maxR + 1, basisSize);
+    for (int i = 0; i < basisSize; i++)
+    {
+        auto [n, l, R, N, mN] = basis[i];
+        nCharMatrix(n-minN, i) = 1.0;
+        lCharMatrix(l, i) = 1.0;
+        RCharMatrix(R, i) = 1.0;
+    }
 
     // generate filename
     std::string path;
     path += GetHomeDirSubfolderPath("remote_home") + "/Masterarbeit/06_StarkMap/03_NO/";
     path += GenerateFilename("NOStarkMap") + ".h5";
     
-    ThreadPool pool(GetNumberOfCalcThreads());
-    StorageThread storageThread(path, state, starkMap.GetBasis(), dE, eField.size());
+    // calculate stark map
+    auto eField = VectorXd::LinSpaced(1500, 0.0, 25.0); // V cm^-1
+
+    // ThreadPool pool(GetNumberOfCalcThreads());
+    ThreadPool pool(1);
+    // StorageThread storageThread(path, state, starkMap.GetBasis(), dE, eField.size());
     ProgressBar progress(eField.size());
 
     for (int i=0; i<eField.size(); i++)
@@ -57,14 +87,25 @@ int main(int argc, const char* argv[])
             auto [energies, states] = starkMap.GetEnergiesAndStates(eField[i] * 100);
             energies /= EnergyInverseCm_v;
             
-            storageThread.AddData(i, eField[i], energies, states);
+            // get character of the states
+            Matrix<double, Dynamic, 3> nlRChar(basisSize, 3);
+
+            MatrixXd tmp1 = nCharMatrix * states;
+            MatrixXd tmp2 = lCharMatrix * states;
+            MatrixXd tmp3 = RCharMatrix * states;
+
+            std::cout << tmp1 << std::endl << std::endl;
+            std::cout << tmp2 << std::endl << std::endl;
+            std::cout << tmp3 << std::endl << std::endl;
+
+            // storageThread.AddData(i, eField[i], energies, states);
             progress.IncrementCount();
         });
     }
 
     progress.WaitUntilFinished();
     std::cout << "Finishing up..." << std::endl;
-    storageThread.WaitUntilFinished();
+    // storageThread.WaitUntilFinished();
 
     std::cout << "Finished. Data saved to " << path << std::endl;
 
