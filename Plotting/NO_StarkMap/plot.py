555cba5b1f7b2e7c2ff0cccc04d6b8d41dd188b6
Philipp Neufeld <philipp.neufeld@protonmail.com>
2022-06-26 22:48:05 +0200

    Why???

--- Plotting/NO_StarkMap/plot.py
+++ Plotting/NO_StarkMap/plot.py
@@ -28,24 +28,84 @@ if __name__ == '__main__':
 
         ax.set_title(path.split("/")[-1])
 
-        # color_palette = np.array([col.to_rgb(f"C{i}") for i in range(10)])
         color_palette = np.array([col.to_rgb(c) for c in ("black", "blue", "red", "green", "purple", "orange", "grey")])
-                
-        print(path)
+              
+        prev_states = None
 
         with h5py.File(path) as file:
-            for datagroup in tqdm.tqdm([file[k] for k in file.keys() if k.isdecimal()]):
 
-                energies = np.array(datagroup["Energies"][::])
-                eField = np.ones_like(energies) * float(datagroup.attrs["Electric_Field"])
-                character = np.array(datagroup["Character"][::])
+            keys = [k for k in file.keys() if k.isdecimal() and k[-1] == '0']
+            
+            efields = np.empty(len(keys))
+            stark_map = np.empty((file[keys[0]]["States"].shape[0], len(keys)))
+            colors = color_palette[np.clip(file[keys[0]]["Character"][:, 2].astype(int), 0, len(color_palette)-1), :]
+
+            for idx, datagroup in enumerate(tqdm.tqdm([file[k] for k in keys])):
+                # load data from file
+                efields[idx] = float(datagroup.attrs["Electric_Field"])
+                eField = np.ones(stark_map.shape[0])*efields[idx]
+
+                energies = np.array(datagroup["Energies"][:, 0])
+                states = np.array(datagroup["States"])
+                character = np.array(datagroup["Character"])
+
+                # reorder basis states to match order of previous configuration
+                if prev_states is not None:
+
+                    av_states = list(range(states.shape[0]))
+                    order = np.empty(energies.shape, dtype=int)
+                    for i in range(states.shape[0]):
+                        s = states[av_states, i]
+                        prev = prev_states[av_states, :][:, av_states]
+
+                        chr_slice = [0, 1, 2]
+                        state_overlap = np.abs(np.matmul(np.transpose(prev), s))
+                        chr_oberlap = np.sum(np.abs(character[av_states, :][:, chr_slice] - prev_chars[i, :][chr_slice]), axis=1)
+
+                        match = np.argmin(chr_oberlap - 0.25*state_overlap)
+                        order[i] = av_states[match]
+                        av_states.remove(av_states[match])
+
+                    # av_states = list(range(states.shape[0]))
+                    # order = np.empty(energies.shape, dtype=int)
+                    # for i in range(states.shape[0]):
+                    #     s = prev_states[av_states, i]
+                    #     prev = states[av_states, av_states]
+                    #     match = np.argmax(np.abs(np.matmul(np.transpose(prev), s)))
+                    #     order[i] = av_states[match]
+                    #     av_states.remove(av_states[match])
+
+                    # order = list(range(states.shape[0]))
+                    # order = np.max(np.abs(np.matmul(np.conj(np.transpose(prev_states)), states)), axis=0)
+                    # order = np.empty(energies.shape, dtype=int)
+                    # for i in range(order.shape[0]):
+                    #     test = np.sum(np.abs(character - prev_states[i, :]), axis=1)
+                    #     order[i] = np.argmin(test)
+
+                    test = len(set(order))
+
+                    energies = energies[order]
+                    states = states[order, :][:, order]
+                    character = character[order, :]
+                    stark_map[:, idx] = energies
+                # else:
+                #     # prev_states = character
+                #     prev_states = states
+                
+                stark_map[:, idx] = energies
 
                 # colors = color_palette[np.mod(character[:, 2].astype(int), len(color_palette)), :]
-                colors = color_palette[np.clip(character[:, 2].astype(int), 0, len(color_palette)-1), :]
+                # colors = color_palette[np.clip(character[:, 3].astype(int), 0, len(color_palette)-1), :]
                 
                 # ax.scatter(eField, energies, 0.5, colors)
-                ax.scatter(eField, energies, 0.75, colors)
-        
+                # ax.scatter(eField, energies, 0.75, colors)
+
+                prev_states = states
+                prev_chars = character
+ 
+        for i in range(stark_map.shape[0]):
+            plt.plot(efields, stark_map[i, :], '-', color=colors[i, :])
+
         ax.set_xlim((0, 25))
         ax.set_ylim((-66.5, -61.5))
         ax.set_xlabel("Electric field (V / cm)")
--- apps/NO_StarkMap/main.cpp
+++ apps/NO_StarkMap/main.cpp
@@ -81,7 +81,7 @@ int main(int argc, const char* argv[])
     
     // calculate stark map
     // auto eField = VectorXd::LinSpaced(1500, 0.0, 25.0); // V cm^-1
-    auto eField = VectorXd::LinSpaced(36, 0.0, 8.0); // V cm^-1
+    auto eField = VectorXd::LinSpaced(180, 0.0, 25.0); // V cm^-1
 
     ThreadPool pool(GetNumberOfCalcThreads());
     StorageThread storageThread(filename, state, starkMap.GetBasis(), dE, eField.size());
