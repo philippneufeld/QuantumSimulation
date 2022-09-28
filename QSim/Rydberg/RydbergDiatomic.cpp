fc2132e3071cd73eff6060db432f0cbc6b71ea31
Philipp Neufeld <philipp.neufeld@protonmail.com>
2022-09-28 23:05:03 +0200

    Tests

--- QSim/Rydberg/RydbergDiatomic.cpp
+++ QSim/Rydberg/RydbergDiatomic.cpp
@@ -18,6 +18,26 @@ namespace QSim
         return n;
     }
 
+    double RydbergDiatomic::GetQuantumDefect(const RydbergDiatomicState_t& state) const
+    {
+        const auto [n, l, R, N, mN] = state;
+
+        const int lmax = this->GetQuantumDefectHcbMaxLambda();
+        int l0 = std::min(l, lmax);
+
+        double defect = 0;
+        for (int Lambda=0; Lambda <= l0; Lambda++)
+        {
+            double aCoeff = GetHcbToHcdCoeff(N, l0, R, Lambda);
+            defect += aCoeff*aCoeff*this->GetQuantumDefectHcb(l0, Lambda);
+        }
+
+        if (l0 < l)
+            defect = 0.0; // ExtrapolateQuantumDefect(defect, l0, l);
+
+        return defect;
+    }
+
     double RydbergDiatomic::GetEnergy(const RydbergDiatomicState_t& state) const
     {
         double energy = 0.0;
@@ -50,35 +70,6 @@ namespace QSim
         potential += this->GetAtomicPotential(r, n, l);
 
         return potential;
-    }
-
-    double RydbergDiatomic::GetDipoleME(const RydbergDiatomicState_t& state1, const RydbergDiatomicState_t& state2) const
-    {
-        const auto [n1, l1, R1, N1, mN1] = state1;
-        const auto [n2, l2, R2, N2, mN2] = state2;
-        
-        // selection rules
-        if (std::abs(l1-l2) != 1 || R1 != R2)
-            return 0.0;
-
-        // angular part
-        double dip = std::sqrt((2*N1+1)*(2*N2+1));
-        dip *= Wigner3j(N1, 1, N2, -mN1, 0, mN2);
-        dip *= Wigner6j(l1, N1, R1, N2, l2, 1);
-        dip *= (N1-mN1+N2+R1+l1+1) % 2 == 0 ? 1.0 : -1.0;
-
-        // check if angular momentum algebra already results 
-        // in a vanishing matrix element
-        if (dip == 0) 
-            return 0.0;
-
-        // radial rydberg matrix element
-        double rmax1 = this->GetIntegrationRange(n1);
-        double rmax2 = this->GetIntegrationRange(n2);
-        dip *= this->GetDipMeRadHelper(state1, state2, 1.0, rmax1, rmax2, s_defaultIntStepsPerOsc);
-        dip *= std::sqrt(std::max(l1, l2)) * (l2 > l1 ? -1 : 1);
-
-        return dip * ElementaryCharge_v;
     }
 
     double RydbergDiatomic::GetSelfDipoleME(const RydbergDiatomicState_t& state1, 
@@ -116,7 +107,111 @@ namespace QSim
         return k * mu * rad * f;
     }
 
-    double RydbergDiatomic::GetHcbToHcdCoeff(int N, int l, int R, int lambda) const
+    double RydbergDiatomic::GetCoreInteractionHcb(int n1, int l1, int R1, 
+        int n2, int l2, int R2, int lambda, int N) const
+    {
+        // no data available
+        if (lambda >= this->GetQuantumDefectHcbMaxLambda())
+            return 0.0;
+
+        double result = 0;
+        
+        // diagonal terms already included
+        // 1 / (n - mu)^2 = 1/n^2 - 2*mu/n^3 + ...
+        if (n1==n2 && l1==l2 && R1==R2)
+            return 0.0;
+
+        // l-l coupling and s-d mixing (dR = even) see Vrakking et. al.
+        // if (std::abs(R1 - R2) % 2 == 0)
+        if (std::abs(R1 - R2) == 2)
+        {
+            // s-d mixing in the Lambda=0 channel
+            if ((l1==0||l1==2) && (l2==0||l2==2) && lambda == 0)
+            {
+                double sdAngle = this->GetSDMixingAngle();
+                double c2 = [](double x){ return x*x; }(std::cos(sdAngle));
+                double s2 = 1 - c2;
+
+                double mus = this->GetQuantumDefectHcb(0, 0);
+                double mud = this->GetQuantumDefectHcb(2, 0);
+
+                if (l1 == 0 && l2 == 0)
+                    result = -(c2*mus + s2*mud);
+                else if (l1 == 2 && l2 == 2)
+                    result = -(c2*mud + s2*mus);
+                else
+                    result = -0.5*std::sin(2*sdAngle) * (mus - mud);
+
+                result /= std::pow((n1-mus)*(n1-mud)*(n2-mus)*(n2-mud), 0.75);
+            }
+            
+            // l-l coupling
+            else if (l1 == l2 && l1 >= lambda && l1 < this->GetQuantumDefectHcbMaxLambda())
+            {
+                double mu = this->GetQuantumDefectHcb(l1, lambda);
+                result = -mu / std::pow((n1-mu)*(n2-mu), 1.5);
+            }
+        }
+
+        // unit correction
+        constexpr double hc = SpeedOfLight_v * PlanckConstant_v;
+        double hcR = hc * this->GetScaledRydbergConstant();
+        return 2 * hcR * result; 
+    }
+
+    double RydbergDiatomic::GetCoreInteractionME(const RydbergDiatomicState_t& state1, 
+            const RydbergDiatomicState_t& state2) const
+    {
+        const auto [n1, l1, R1, N1, mN1] = state1;
+        const auto [n2, l2, R2, N2, mN2] = state2;
+
+        // diagonal in N and mN
+        if (N1 != N2 || mN1 != mN2)
+            return 0.0;
+
+        // convert from Hund's case (b) to (d)
+        double result = 0;
+        for (int lambda = 0; lambda <= this->GetQuantumDefectHcbMaxLambda(); lambda++)
+        {
+            double hcb = this->GetCoreInteractionHcb(n1, l1, R1, n2, l2, R2, lambda, N1);
+            double a1 = this->GetHcbToHcdCoeff(N1, l1, R1, lambda);
+            double a2 = this->GetHcbToHcdCoeff(N2, l2, R2, lambda);
+            result += hcb * a1 * a2;
+        }
+
+        return result;
+    }
+
+    double RydbergDiatomic::GetDipoleME(const RydbergDiatomicState_t& state1, const RydbergDiatomicState_t& state2) const
+    {
+        const auto [n1, l1, R1, N1, mN1] = state1;
+        const auto [n2, l2, R2, N2, mN2] = state2;
+        
+        // selection rules
+        if (std::abs(l1-l2) != 1 || R1 != R2)
+            return 0.0;
+
+        // angular part
+        double dip = std::sqrt((2*N1+1)*(2*N2+1));
+        dip *= Wigner3j(N1, 1, N2, -mN1, 0, mN2);
+        dip *= Wigner6j(l1, N1, R1, N2, l2, 1);
+        dip *= (N1-mN1+N2+R1+l1+1) % 2 == 0 ? 1.0 : -1.0;
+
+        // check if angular momentum algebra already results 
+        // in a vanishing matrix element
+        if (dip == 0) 
+            return 0.0;
+
+        // radial rydberg matrix element
+        double rmax1 = this->GetIntegrationRange(n1);
+        double rmax2 = this->GetIntegrationRange(n2);
+        dip *= this->GetDipMeRadHelper(state1, state2, 1.0, rmax1, rmax2, s_defaultIntStepsPerOsc);
+        dip *= std::sqrt(std::max(l1, l2)) * (l2 > l1 ? -1 : 1);
+
+        return dip * ElementaryCharge_v;
+    }
+
+    double RydbergDiatomic::GetHcbToHcdCoeff(int N, int l, int R, int lambda)
     {
         double kronL0 = (lambda == 0 ? 1 : 0);
         double coeff = std::sqrt((2*R + 1) * 2.0 / (1 + kronL0));
@@ -125,37 +220,13 @@ namespace QSim
         return coeff;
     }
 
+
     //
     // NitricOxide
     //
 
     NitricOxide::NitricOxide() 
         : RydbergDiatomic(30.006 * AtomicMassUnit_v) {}
-
-    double NitricOxide::GetQuantumDefect(const RydbergDiatomicState_t& state) const
-    {
-        const auto [n, l, R, N, mN] = state;
-
-        const static std::array<const double*, 4> quantumDefects = {
-            s_l0quantumDefects.data(), s_l1quantumDefects.data(),
-            s_l2quantumDefects.data(), s_l3quantumDefects.data()
-        };
-
-        constexpr int lmax = quantumDefects.size() - 1;
-        int l0 = std::min(l, lmax);
-
-        double defect = 0;
-        for (int Lambda=0; Lambda <= l0; Lambda++)
-        {
-            double aCoeff = GetHcbToHcdCoeff(N, l0, R, Lambda);
-            defect += aCoeff*aCoeff*quantumDefects[l0][Lambda];
-        }
-
-        if (l0 < l)
-            defect = 0.0; // ExtrapolateQuantumDefect(defect, l0, l);
-
-        return defect;
-    }
 
     double NitricOxide::GetRotationalConstant() const
     {
@@ -175,84 +246,29 @@ namespace QSim
         return 0.4 * Debye_v;
     }
 
-    double NitricOxide::GetCoreInteractionME(const RydbergDiatomicState_t& state1, 
-            const RydbergDiatomicState_t& state2) const
+    double NitricOxide::GetSDMixingAngle() const
     {
-        const auto [n1, l1, R1, N1, mN1] = state1;
-        const auto [n2, l2, R2, N2, mN2] = state2;
-
-        // diagonal in N and mN
-        if (N1 != N2 || mN1 != mN2)
-            return 0.0;
-
-        // convert from Hund's case (b) to (d)
-        double result = 0;
-        for (int lambda = 0; lambda <= s_quantumDefectsLmax; lambda++)
-        {
-            double hcb = this->GetCoreInteractionHcb(n1, l1, R1, n2, l2, R2, lambda, N1);
-            double a1 = this->GetHcbToHcdCoeff(N1, l1, R1, lambda);
-            double a2 = this->GetHcbToHcdCoeff(N2, l2, R2, lambda);
-            result += hcb * a1 * a2;
-        }
-
-        return result;
+        return -38.7 * Pi_v / 180.0;
     }
 
-    double NitricOxide::GetCoreInteractionHcb(int n1, int l1, int R1, int n2, int l2, int R2, int lambda, int N) const
+    double NitricOxide::GetQuantumDefectHcb(int l, int Lambda) const
     {
+        // DOI: 10.1039/d1cp01930a
+        constexpr static std::array<double, 1> s_l0quantumDefects = { 0.210 };
+        constexpr static std::array<double, 2> s_l1quantumDefects = { 0.7038, 0.7410 };
+        constexpr static std::array<double, 3> s_l2quantumDefects = { 0.050, -0.053, 0.089 };
+        constexpr static std::array<double, 4> s_l3quantumDefects = { 0.0182, 0.0172, 0.00128, 0.0057 };
+        
         const static std::array<const double*, 4> quantumDefects = {
             s_l0quantumDefects.data(), s_l1quantumDefects.data(),
             s_l2quantumDefects.data(), s_l3quantumDefects.data()
         };
-
-        // no data available
-        if (lambda >= quantumDefects.size())
-            return 0.0;
-
-        double result = 0;
-        
-        // diagonal terms already included
-        // 1 / (n - mu)^2 = 1/n^2 - 2*mu/n^3 + ...
-        if (n1==n2 && l1==l2 && R1==R2)
-            return 0.0;
-
-        // l-l coupling and s-d mixing (dR = even) see Vrakking et. al.
-        // if (std::abs(R1 - R2) % 2 == 0)
-        if (std::abs(R1 - R2) == 2)
-        {
-            // s-d mixing in the Lambda=0 channel
-            if ((l1==0||l1==2) && (l2==0||l2==2) && lambda == 0)
-            {
-                constexpr double sdAngle = -38.7 * Pi_v / 180.0;
-                double c2 = [](double x){ return x*x; }(std::cos(sdAngle));
-                double s2 = 1 - c2;
-
-                double mus = quantumDefects[0][0];
-                double mud = quantumDefects[2][0];
-
-                if (l1 == 0 && l2 == 0)
-                    result = -(c2*mus + s2*mud);
-                else if (l1 == 2 && l2 == 2)
-                    result = -(c2*mud + s2*mus);
-                else
-                    result = -0.5*std::sin(2*sdAngle) * (mus + mud); // TODO: mus - mud
-
-                result /= std::pow((n1-mus)*(n1-mud)*(n2-mus)*(n2-mud), 0.75);
-            }
-            
-            // l-l coupling
-            else if (l1 == l2 && l1 >= lambda && l1 < quantumDefects.size())
-            {
-                double mu = quantumDefects[l1][lambda];
-                result = -mu / std::pow((n1-mu)*(n2-mu), 1.5);
-            }
-        }
-
-        // unit correction
-        constexpr double hc = SpeedOfLight_v * PlanckConstant_v;
-        double hcR = hc * this->GetScaledRydbergConstant();
-        return 2 * hcR * result; 
+        return quantumDefects[l][Lambda];
     }
 
+    int NitricOxide::GetQuantumDefectHcbMaxLambda() const
+    {
+        return 3;
+    }
 
 }
--- QSim/Rydberg/RydbergDiatomic.h
+++ QSim/Rydberg/RydbergDiatomic.h
@@ -29,47 +29,44 @@ namespace QSim
         virtual double GetRotationalConstant() const = 0;
         virtual double GetCentrifugalDistConstant() const = 0;
         virtual double GetCoreDipoleMoment() const = 0;
+        virtual double GetSDMixingAngle() const = 0;
+        virtual double GetQuantumDefectHcb(int l, int Lambda) const = 0;
+        virtual int GetQuantumDefectHcbMaxLambda() const = 0;
 
-        // coefficients needed for the multielectron correction
-        virtual double GetCoreInteractionME(const RydbergDiatomicState_t& state1, 
-            const RydbergDiatomicState_t& state2) const = 0;
+        // quantum defects
+        virtual double GetQuantumDefect(const RydbergDiatomicState_t& state) const override;
 
+        // energy and potential
         virtual double GetEnergy(const RydbergDiatomicState_t& state) const override;
         virtual double GetPotential(double r, const RydbergDiatomicState_t& state) const override;
 
-        double GetEnergyApprox(const RydbergDiatomicState_t& state) const;
-        
-        virtual double GetDipoleME(const RydbergDiatomicState_t& state1, 
-            const RydbergDiatomicState_t& state2) const override;
-
+        // hamiltonian terms
         double GetSelfDipoleME(const RydbergDiatomicState_t& state1, 
             const RydbergDiatomicState_t& state2) const;
+        double GetCoreInteractionHcb(int n1, int l1, int R1, int n2, 
+            int l2, int R2, int lambda, int N) const;
+        double GetCoreInteractionME(const RydbergDiatomicState_t& state1, 
+            const RydbergDiatomicState_t& state2) const;
         
-    protected:
-        double GetHcbToHcdCoeff(int N, int l, int R, int lambda) const;
+        // dipole matrix element
+        virtual double GetDipoleME(const RydbergDiatomicState_t& state1, 
+            const RydbergDiatomicState_t& state2) const override;
+        
+        // transformation coefficients
+        static double GetHcbToHcdCoeff(int N, int l, int R, int lambda);
     };
 
     class NitricOxide : public RydbergDiatomic
     {
-    private:
-        // DOI: 10.1039/d1cp01930a
-        constexpr static std::array<double, 1> s_l0quantumDefects = { 0.210 };
-        constexpr static std::array<double, 2> s_l1quantumDefects = { 0.7038, 0.7410 };
-        constexpr static std::array<double, 3> s_l2quantumDefects = { 0.050, -0.053, 0.089 };
-        constexpr static std::array<double, 4> s_l3quantumDefects = { 0.0182, 0.0172, 0.00128, 0.0057 };
-        constexpr static int s_quantumDefectsLmax = 3;
     public:
         NitricOxide();
         
-        virtual double GetQuantumDefect(const RydbergDiatomicState_t& state) const override;
         virtual double GetRotationalConstant() const override;
         virtual double GetCentrifugalDistConstant() const override;
         virtual double GetCoreDipoleMoment() const override;
-
-        virtual double GetCoreInteractionME(const RydbergDiatomicState_t& state1, 
-            const RydbergDiatomicState_t& state2) const override;
-        double GetCoreInteractionHcb(int n1, int l1, int R1, int n2, int l2, int R2, int lambda, int N) const;
-        
+        virtual double GetSDMixingAngle() const override;
+        virtual double GetQuantumDefectHcb(int l, int Lambda) const override;
+        virtual int GetQuantumDefectHcbMaxLambda() const override; 
     };
 
 }
